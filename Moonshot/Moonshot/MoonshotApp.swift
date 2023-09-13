88beb962e9a572bef66d62b257c4bc163f679f38
Anna <35114047+AKoneva@users.noreply.github.com>
2023-09-13 13:00:22 +0900

    init

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Анна Перехрест  on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
